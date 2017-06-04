92dc0369ecb71f01ee3e5f4b18099c136a59cd2b
chvck <chvckd@gmail.com>
2017-06-04 22:02:48 +0100

    Fix tests

--- client/dave_client_test.go
+++ client/dave_client_test.go
@@ -1,27 +1,42 @@
-package client
+package client_test
 
 import (
 	"net/http"
 	"testing"
 
-	"github.com/Gamebuildr/Dave/pkg/scaler"
+	"os"
+
+	"github.com/Gamebuildr/Dave/client"
+	"github.com/Gamebuildr/Dave/pkg/config"
+	"github.com/Gamebuildr/Dave/pkg/watcher"
 )
 
 type MockWatcher struct {
+	MessageID    string
+	Message      string
 	messagecount int
 }
 
-func (watcher MockWatcher) ReadNextMessage(url string) (int, error) {
-	return watcher.messagecount, nil
+func (mockWatcher MockWatcher) ReadNextMessage(url string) (*watcher.MessageInfo, error) {
+	return &watcher.MessageInfo{
+		MessageID: mockWatcher.MessageID,
+		Message:   mockWatcher.Message,
+	}, nil
 }
 
-func (watcher MockWatcher) Setup() {
+func (mockWatcher MockWatcher) DeleteMessage(messageID string, url string) error {
+	return nil
+}
+
+func (mockWatcher MockWatcher) Setup() {
 }
 
 type MockScaler struct {
 	GetLoadHit   bool
 	AddSystemHit bool
 	loadcount    int
+	addLoadURL   string
+	getLoadURL   string
 }
 
 func (system *MockScaler) GetSystemLoad() (int, error) {
@@ -29,10 +44,15 @@ func (system *MockScaler) GetSystemLoad() (int, error) {
 	return system.loadcount, nil
 }
 
-func (system *MockScaler) AddSystemLoad() (*http.Response, error) {
+func (system *MockScaler) AddSystemLoad(message string) (*http.Response, error) {
 	system.AddSystemHit = true
 
 	return &http.Response{StatusCode: http.StatusOK}, nil
+}
+
+func (system *MockScaler) SetLoadURLs(loadAPIUrl string, addLoadAPIUrl string) {
+	system.getLoadURL = loadAPIUrl
+	system.addLoadURL = addLoadAPIUrl
 }
 
 type MockLogger struct{}
@@ -46,12 +66,12 @@ func (log MockLogger) Error(message string) string {
 }
 
 func TestClientDoesNotRunWhenMessagesZero(t *testing.T) {
-	mockClient := DaveClient{}
+	mockClient := client.DaveClient{}
 	mockClient.Log = MockLogger{}
 	scalerSystem := MockScaler{}
-	scaler := scaler.ScalableSystem{System: &scalerSystem}
-	mockClient.Watcher.Queue = MockWatcher{messagecount: 0}
-	mockClient.RunClient(&scaler, "mock/url")
+	mockClient.Watcher.Queue = MockWatcher{messagecount: 0, MessageID: "", Message: "{}"}
+	mockClient.ClientScaler = &scalerSystem
+	mockClient.RunClient(client.GogetaSubsystem, "mock/url", 1)
 
 	if scalerSystem.GetLoadHit != false {
 		t.Errorf("Expected GetSystemLoad to not be called")
@@ -62,15 +82,12 @@ func TestClientDoesNotRunWhenMessagesZero(t *testing.T) {
 }
 
 func TestClientDoesNotRunOverMaxLoad(t *testing.T) {
-	mockClient := DaveClient{}
+	mockClient := client.DaveClient{}
 	mockClient.Log = MockLogger{}
 	scalerSystem := MockScaler{loadcount: 5}
-	scaler := scaler.ScalableSystem{
-		System:  &scalerSystem,
-		MaxLoad: 4,
-	}
-	mockClient.Watcher.Queue = MockWatcher{messagecount: 1}
-	mockClient.RunClient(&scaler, "mock/url")
+	mockClient.Watcher.Queue = MockWatcher{messagecount: 1, MessageID: "1", Message: "{}"}
+	mockClient.ClientScaler = &scalerSystem
+	mockClient.RunClient(client.GogetaSubsystem, "mock/url", 4)
 
 	if scalerSystem.GetLoadHit != true {
 		t.Errorf("Expected GetSystemLoad to be called")
@@ -80,21 +97,54 @@ func TestClientDoesNotRunOverMaxLoad(t *testing.T) {
 	}
 }
 
-func TestClientAddsToLoadWhenLoadUnderMax(t *testing.T) {
-	mockClient := DaveClient{}
+func TestClientAddsToLoadWhenLoadUnderMaxGogetaImage(t *testing.T) {
+	mockClient := client.DaveClient{}
 	mockClient.Log = MockLogger{}
 	scalerSystem := MockScaler{loadcount: 1}
-	scaler := scaler.ScalableSystem{
-		System:  &scalerSystem,
-		MaxLoad: 4,
-	}
-	mockClient.Watcher.Queue = MockWatcher{messagecount: 1}
-	mockClient.RunClient(&scaler, "mock/url")
+	mockClient.Watcher.Queue = MockWatcher{messagecount: 1, MessageID: "1", Message: "{}"}
+	mockClient.ClientScaler = &scalerSystem
+	halAPI := "http://localhost/"
+	os.Setenv(config.HalGogetaAPI, halAPI)
+	mockClient.RunClient(client.GogetaSubsystem, "mock/url", 4)
 
+	countAPI := halAPI + "api/container/count"
+	runAPI := halAPI + "api/container/run?image=gcr.io/gamebuildr-151415/gamebuildr-gogeta"
 	if scalerSystem.GetLoadHit != true {
 		t.Errorf("Expected GetSystemLoad to be called")
 	}
 	if scalerSystem.AddSystemHit != true {
 		t.Errorf("Expected AddSystemLoad to not be called")
+	}
+	if scalerSystem.getLoadURL != countAPI {
+		t.Errorf("Expected getLoadURL to be %v, got %v", countAPI, scalerSystem.getLoadURL)
+	}
+	if scalerSystem.addLoadURL != runAPI {
+		t.Errorf("Expected getLoadURL to be %v, got %v", runAPI, scalerSystem.addLoadURL)
+	}
+}
+
+func TestClientAddsToLoadWhenLoadUnderMaxMrRobotImage(t *testing.T) {
+	mockClient := client.DaveClient{}
+	mockClient.Log = MockLogger{}
+	scalerSystem := MockScaler{loadcount: 1}
+	mockClient.Watcher.Queue = MockWatcher{messagecount: 1, MessageID: "1", Message: `{"enginename": "godot engine", "engineversion": "2.1"}`}
+	mockClient.ClientScaler = &scalerSystem
+	halAPI := "http://localhost/"
+	os.Setenv(config.HalMrRobotAPI, halAPI)
+	mockClient.RunClient(client.MrRobotSubsystem, "mock/url", 4)
+
+	countAPI := halAPI + "api/container/count"
+	runAPI := halAPI + "api/container/run?image=gcr.io/gamebuildr-151415/mr.robot-godot-2.1.2"
+	if scalerSystem.GetLoadHit != true {
+		t.Errorf("Expected GetSystemLoad to be called")
+	}
+	if scalerSystem.AddSystemHit != true {
+		t.Errorf("Expected AddSystemLoad to not be called")
+	}
+	if scalerSystem.getLoadURL != countAPI {
+		t.Errorf("Expected getLoadURL to be %v, got %v", countAPI, scalerSystem.getLoadURL)
+	}
+	if scalerSystem.addLoadURL != runAPI {
+		t.Errorf("Expected getLoadURL to be %v, got %v", runAPI, scalerSystem.addLoadURL)
 	}
 }
--- pkg/scaler/scaler_interface.go
+++ pkg/scaler/scaler_interface.go
@@ -5,4 +5,5 @@ import "net/http"
 type System interface {
 	GetSystemLoad() (int, error)
 	AddSystemLoad(message string) (*http.Response, error)
+	SetLoadURLs(loadAPIUrl string, addLoadAPIUrl string)
 }
